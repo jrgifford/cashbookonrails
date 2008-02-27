6c2dad67af46288f7e2bb53c01ee00b6def75dc2
trabelo <trabelo@02569943-1347-0410-9572-df3bbcc21079>
2008-02-27 13:53:26 +0000

    Retirado alguns links desnecessarios na pagina principal e na pagina de relatorios.

--- cashbookonrails/app/controllers/reports_controller.rb
+++ cashbookonrails/app/controllers/reports_controller.rb
@@ -3,4 +3,24 @@ class ReportsController < ApplicationController
   
   def index    
   end
+
+  def spending_by_category 
+    
+  end
+
+  def spending_by_payee 
+    
+  end
+  
+  def credit_card_debt 
+    
+  end
+  
+  def top_five_expense_categories 
+    
+  end
+  
+  def pending_transactions 
+    
+  end
 end
